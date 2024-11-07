c7a4d43d813f4ddbdb0c0709561e3367ccb5bf1e
Bhumit <javiyabhumit54@gmail.com>
2024-11-07 21:22:05 +0530

    portfolio added

--- public/assets/inedx.ts
+++ public/assets/inedx.ts
@@ -3,5 +3,6 @@ import profileImg from "./images/profile.jpg";
 import todoImg from "./images/todo.png"
 import playgridImg from "./images/playgrid.png"
 import devImg from "./images/dev.png"
+import bhumitImg from "./images/bhumit.png"
 
-export { logo, profileImg, todoImg, playgridImg, devImg };
+export { logo, profileImg, todoImg, playgridImg, devImg, bhumitImg };
